e2d1a5d53da9ac1447d71aab91584475cc5ff379
Joshua Oyewole <joshpp2013@gmail.com>
2020-10-12 23:30:40 +0100

    Delete Component #2 bug resolved

--- src/components/Delete.js
+++ src/components/Delete.js
@@ -2,14 +2,15 @@ import React from 'react'
 
 function Delete() {
 
-    const delItem =(e) =>{
-        console.log('Clicked');
+     const delItem = (e)=>{
         
         const type_income = 'income';
         const type_expense = 'expense';
-        const parentItem = e.target.parentNode.parentNode.parentNode.parentNode;
-        const childItem = e.target.parentNode.parentNode.parentNode;
-        parentItem.removeChild(childItem)
+        const parentItem = e.target.parentNode.parentNode.parentNode.parentNode.parentNode;
+        const childItem = e.target.parentNode.parentNode.parentNode.parentNode;
+    
+        
+        parentItem.removeChild(childItem); 
         const parentItem__className =  parentItem.className.includes('expense');
         
        if (parentItem__className) {
@@ -25,33 +26,34 @@ function Delete() {
             
             //Remove todo from localStorage
             removeList(childItem,incomes,type_income);
-        }
-      
+        } 
     }
-
-     const removeList = (list,listArr, type) =>{
+        const removeList = (list,listArr, type) => {
      
-        //Accessing the ID attribute of the current list element
-        const listID = list.id;
-        //Getting the id number of the current list element
-        const id = listID.split('-')[1];
-        
-        //Checking if its the last item in the List inorder to returning an empty array to the localStorage
-        if (listArr.length === 1){
-            listArr.splice(0,1);
-            localStorage.removeItem(type)   
-        }        
-        else{
-            listArr.splice(id,1);
-            localStorage.setItem(type, JSON.stringify(listArr));
-        }
-    }
+            //Accessing the ID attribute of the current list element
+            const listID = list.id;
+            //Getting the id number of the current list element
+            const id = listID.split('-')[1];
+            
+            //Checking if its the last item in the List inorder to returning an empty array to the localStorage
+            if (listArr.length === 1 && !null){
+                listArr.splice(0,1);
+                localStorage.removeItem(type);   
+            }   
+
+            else{
+                listArr.splice(id,1);
+                localStorage.setItem(type, JSON.stringify(listArr));
+            } 
+    } 
+    window.addEventListener('click', delItem)
     return (
-          <i 
+        <i 
             style={{color: 'red'}} 
-            onClick={delItem} 
-            className="fas fa-trash">
-          </i>
-            )
-    }   
+            onClick={delItem}
+            className="fas fa-trash"> 
+        </i>
+    )
+} 
+
 export default Delete
